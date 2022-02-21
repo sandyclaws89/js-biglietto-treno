6b96e6a472ec09048a62fcb474a957247c5e8d99
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-02-21 18:19:45 +0100

    Esercizio completato con gestione numeri decimali

--- js/script.js
+++ js/script.js
@@ -1,15 +1,44 @@
 let age= prompt(`Inserire l'età`);
 let km= prompt(`Inserire il Numero di Km da percorrere`);
 let cost = (km * 0.21);
+let disc20=0;
+let disc40=0;
+let tot=1;
+
 
 if (age<18){
-let disc20 = (cost * 0.20);
-let tot = (cost - disc20);
+disc20 = (cost * 0.20);
+tot = (cost - disc20);
 }  else if (age>=65){
-let disc40 = (cost * 0.40);
-let tot = (cost - disc40);
-
+ disc40 = (cost * 0.40);
+ tot = (cost - disc40);
+}else{
+    tot=cost;
 }
+
+tot= tot.toFixed(2);
+
+
 let price = document.getElementById(`result`) 
 price.innerHTML = `Il costo del biglietto è di ${tot} €`;
 
+
+
+// // METODO 1 PIU PRECISO 
+
+// // NUMERO DA ARROTONDARE
+// let numb= 212421434.533423131231;
+// // CALCOLO PER ARROTONDARE IL NUMERO 
+// let rounded = Math.round((numb + Number.EPSILON) * 100) / 100;
+// // STAMPA NUMERO
+// console.log(rounded);
+
+
+
+// // METODO 2 MENO PRECISO 
+
+// // NUMERO DA ARROTONDARE 
+// let numb = 12312214.124124124;
+
+// // ISTRUZIONE PER ARROTONDARE A 2 CIFRE DOPO LA VIRGOLA 
+// numb = numb.toFixed(2);
