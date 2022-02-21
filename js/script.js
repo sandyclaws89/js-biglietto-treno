f1bb86fbd071b7f7ca4031059ee220e84131bebc
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-02-21 15:10:30 +0100

    Test vari

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let Myvar = prompt
+
+let str1 ="ciao";
+let str2 ="a tutti;";
+console.log (str1 + str2);
+
+// I BACKTICK PERMETTONO AL TESTO DI ANDARE A CAPO O DI SOMMARE 2 O PIU VARIABILI CON IL SIMBOLO DEL DOLLARO E LA PARENTESI GRAFFA SENZA BISOGNO DI METTERE IL + E ALTRI APICI
+let BacktickTest = `ciao io sono un testo 
+e posso andare a capo ${str1} ${str2}`
+console.log (BacktickTest);
+
+// IL SIMBOLO PERCENTUALE DA IL RESTO DELLA DIVISONE 
+console.log (5 % 2);
+
