e539cb408c1cb279411407d64d86f4b25aa9415b
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-02-21 16:35:26 +0100

    Impostazione

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-let Myvar = prompt
+let Age= prompt(`Inserire l'età`);
+let Km= prompt(`Inserire il Numero di Km da percorrere`);
+let Cost = (Km *0.21);
 
-let str1 ="ciao";
-let str2 ="a tutti;";
-console.log (str1 + str2);
-
-// I BACKTICK PERMETTONO AL TESTO DI ANDARE A CAPO O DI SOMMARE 2 O PIU VARIABILI CON IL SIMBOLO DEL DOLLARO E LA PARENTESI GRAFFA SENZA BISOGNO DI METTERE IL + E ALTRI APICI
-let BacktickTest = `ciao io sono un testo 
-e posso andare a capo ${str1} ${str2}`
-console.log (BacktickTest);
-
-// IL SIMBOLO PERCENTUALE DA IL RESTO DELLA DIVISONE 
-console.log (5 % 2);
-
+if (Age<18){
+Cost= (Cost * 0.20);
+}  else if (Age>=65){
+Cost= (Cost * 0.40)
+}
+let Cost = document.getElementById(`MyVar`) 
+Cost.innerHTML = `Il costo del biglietto è di ${Cost} €`;
